6172e425b331f553d87c65f80c09fc7b8db0a34a
Bo Latt <bolattny@gmail.com>
2023-02-08 01:01:33 -0500

    crud complete for cars

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -6,9 +6,65 @@ cars.get("/", async (req, res) => {
   try {
     const cars = await db.any("SELECT * FROM cars");
     console.log(cars);
-    res.send(cars);
+    res.json(cars);
   } catch (error) {
     console.log(error.message);
+    res.status(400).json({ error: "something went wrong" });
+  }
+});
+
+cars.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const car = await db.any("SELECT * FROM cars WHERE id =$1", id);
+    console.log(car);
+    res.json(car);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: "something went wrong" });
+  }
+});
+
+cars.post("/", async (req, res) => {
+  try {
+    const { name, quantity, year, color, image_link, collection_id } = req.body;
+    const createdCar = await db.one(
+      "INSERT INTO cars(name,quantity,year,color,image_link,collection_id) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+      [name, quantity, year, color, image_link, collection_id]
+    );
+    console.log(createdCar);
+    res.json(createdCar);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: "something went wrong" });
+  }
+});
+
+cars.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, quantity, year, color, image_link, collection_id } = req.body;
+    const updatedCar = await db.one(
+      "UPDATE cars SET name = $1, quantity = $2, year = $3, color=$4, image_link=$5,collection_id=$6 WHERE id=$7 RETURNING *",
+      [name, quantity, year, color, image_link, collection_id, id]
+    );
+    console.log(updatedCar);
+    res.json(updatedCar);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: "something went wrong" });
+  }
+});
+
+cars.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCar = await db.one(
+      "DELETE FROM cars WHERE id= $1 RETURNING *",
+      id
+    );
+    res.json(deletedCar);
+  } catch (error) {
     res.status(400).json({ error: "something went wrong" });
   }
 });
